82b06a43c5c8261753dc31b8b78c5ef30293da7c
Công Thế <thedc.it.94@gmail.com>
2025-04-17 23:09:51 +0700

    add login form

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ *File name : login.blade.php  / Date: 4/17/2025 - 9:29 PM
+ *Code Owner: Tke / Phone: 0367313134 / Email: thedc.it.94@gmail.com
+ */
---
+++ routes/auth.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ *File name : auth.php  / Date: 4/17/2025 - 9:27 PM
+ *Code Owner: Tke / Phone: 0367313134 / Email: thedc.it.94@gmail.com
+ */
