3e17476eaeec72b7f79915136332ca0529749350
Công Thế <thedc.it.94@gmail.com>
2025-04-18 23:59:51 +0700

    Update login and register

---
+++ resources/lang/vi/auth.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ *File name : auth.php  / Date: 4/18/2025 - 10:27 PM
+ *Code Owner: Tke / Phone: 0367313134 / Email: thedc.it.94@gmail.com
+ */
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ *File name : register.blade.php  / Date: 4/18/2025 - 10:47 PM
+ *Code Owner: Tke / Phone: 0367313134 / Email: thedc.it.94@gmail.com
+ */
